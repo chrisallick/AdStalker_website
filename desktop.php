695fde396cf26710c30650d875028ae33fb90866
Chris Allick <chrisallick@gmail.com>
2016-01-23 14:46:09 -0800

    major design update and fixed social

--- desktop.php
+++ desktop.php
@@ -1,50 +1,50 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Ad Stalker</title>
+        <title>AdStalker</title>
 
         <meta charset="utf-8">
-        <meta name="description" content="Ad Stalker">
+        <meta name="description" content="AdStalker">
         <meta name="keywords" content="">
 
-        <meta property="og:title" content="Ad Stalker" />
+        <meta property="og:title" content="AdStalker" />
         <meta property="og:type" content="article" />
         <meta property="og:image" content="http://getadstalker.com/img/share_this.png" />
         <meta property="og:url" content="http://getadstalker.com/" />
-        <meta property="og:description" content="Creepin while on the clock." />
+        <meta property="og:description" content="Stalk Efficiently" />
+
+        <meta name="twitter:card" value="summary">
+        <meta name="twitter:url" content="http://getadstalker.com"> 
+        <meta name="twitter:domain" content="http://getadstalker.com">
+        <meta name="twitter:title" content="AdStalker">
+        <meta name="twitter:description" content="AdStalker: Stalk Efficiently http://getadstalker.com">
+        <meta name="twitter:image" content="http://getadstalker.com/img/share_this.png" />
         
         <link href='./css/reset.css' media='all' rel='stylesheet' type='text/css' />
         <link href='./css/main.css' media='all' rel='stylesheet' type='text/css' />
-
-        <script>
-            // var tablet = false;
-            // <?php
-            //     if( $detect->isTablet() ) {
-            //         echo "tablet = true;";
-            //     }
-            // ?>
-        </script>
 
         <script src='http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js' type='text/javascript'></script>
         <script src='./js/main.js' type='text/javascript'></script>
         <script src='./js/plugins.js' type='text/javascript'></script>
         
         <script type="text/javascript">
-            // var _gaq = _gaq || [];
-            // _gaq.push(['_setAccount', 'UA-55463424-1']);
-            // _gaq.push(['_trackPageview']);
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-72804926-1']);
+            _gaq.push(['_trackPageview']);
 
-            // (function() {
-            // var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            // ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            // var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            // })();
+            (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
         </script>
     </head>
     <body>
         <div id="wrapper">
+            <a id="twitter" class="social" href="javascript:(function(){window.twttr=window.twttr||{};var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,F=document,E;if(C>A){G=Math.round((C/2)-(A/2))}window.twttr.shareWin=window.open('https://twitter.com/intent/tweet?text=AdStalker: Stalk Efficiently http://getadstalker.com','','left='+H+',top='+G+',width='+D+',height='+A+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');E=F.createElement('script');E.src='http://platform.twitter.com/widgets.js';F.getElementsByTagName('head')[0].appendChild(E)}());" target="_blank"><img src="./img/twitter.png" /></a>
+            <a id="facebook" class="social" href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent('http://getadstalker.com'),'facebook-share-dialog','width=626,height=436'); return false;" target="_blank"><img src="./img/facebook.png" /></a>
             <div id="logo"></div>
-            <h1 id="tagline">Replace ads with the objects of your desire.</h1>
+            <h1 id="tagline">AdStalker: Stalk Efficiently</h1>
             <img id="gif" src="./img/gif_fpo.png" />
             <img id="getbutton" src="./img/getbutton.png" />
         </div>
--- mobile.php
+++ mobile.php
@@ -1,34 +1,50 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Boiler PHP</title>
-
-        <meta name="viewport" content="user-scalable=no, minimal-ui">
+        <title>Ad Stalker</title>
 
         <meta charset="utf-8">
+        <meta name="description" content="Ad Stalker">
+        <meta name="keywords" content="">
 
-        <link href='./css/reset.css' media='all' rel='stylesheet' type='text/css' />
-        <link href='./css/mobile.css' media='all' rel='stylesheet' type='text/css' />
+        <meta property="og:title" content="Ad Stalker" />
+        <meta property="og:type" content="article" />
+        <meta property="og:image" content="http://getadstalker.com/img/share_this.png" />
+        <meta property="og:url" content="http://getadstalker.com/" />
+        <meta property="og:description" content="Creepin while on the clock." />
+
+        <meta name="twitter:card" value="summary">
+        <meta name="twitter:url" content="http://getadstalker.com"> 
+        <meta name="twitter:domain" content="http://getadstalker.com">
+        <meta name="twitter:title" content="Ad Stalker">
+        <meta name="twitter:description" content="Creep while on the clock http://getadstalker.com">
+        <meta name="twitter:image" content="http://getadstalker.com/img/share_this.png" />
         
+        <link href='./css/reset.css' media='all' rel='stylesheet' type='text/css' />
+        <link href='./css/main.css' media='all' rel='stylesheet' type='text/css' />
+
         <script src='http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js' type='text/javascript'></script>
-        <script src='./js/mobile.js' type='text/javascript'></script>
-
+        <script src='./js/main.js' type='text/javascript'></script>
+        <script src='./js/plugins.js' type='text/javascript'></script>
+        
         <script type="text/javascript">
-            // var _gaq = _gaq || [];
-            // _gaq.push(['_setAccount', 'UA-55463424-1']);
-            // _gaq.push(['_trackPageview']);
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-72804926-1']);
+            _gaq.push(['_trackPageview']);
 
-            // (function() {
-            // var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            // ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            // var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            // })();
+            (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
         </script>
     </head>
     <body>
         <div id="wrapper">
-            
-
+            <div id="logo"></div>
+            <h1 id="tagline">Replace ads with the objects of your desire.</h1>
+            <img id="gif" src="./img/gif_fpo.png" />
+            <img id="getbutton" src="./img/getbutton.png" />
         </div>
     </body>
 </html>
