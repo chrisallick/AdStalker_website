3e8f672fe85c8e92fcbad2ef004ec5c0da427d1b
Chris Allick <chrisallick@gmail.com>
2016-01-23 20:24:35 -0800

    remove perido

--- mobile.php
+++ mobile.php
@@ -48,7 +48,7 @@
             <a id="twitter" class="social" href="javascript:(function(){window.twttr=window.twttr||{};var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,F=document,E;if(C>A){G=Math.round((C/2)-(A/2))}window.twttr.shareWin=window.open('https://twitter.com/intent/tweet?text=AdStalker: Stalk Efficiently http://getadstalker.com','','left='+H+',top='+G+',width='+D+',height='+A+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');E=F.createElement('script');E.src='http://platform.twitter.com/widgets.js';F.getElementsByTagName('head')[0].appendChild(E)}());" target="_blank"><img src="./img/twitter.png" /></a>
             <a id="facebook" class="social" href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent('http://getadstalker.com'),'facebook-share-dialog','width=626,height=436'); return false;" target="_blank"><img src="./img/facebook.png" /></a>
             <div id="logo"></div>
-            <h1 id="tagline">Replace ads with photos<br/>from your crush's Instagram.<br/><br/>AdStalker: Stalk Efficiently.</h1>
+            <h1 id="tagline">Replace ads with photos<br/>from your crush's Instagram.<br/><br/>AdStalker: Stalk Efficiently</h1>
             <div id="gif"></div>
         </div>
     </body>
